f20aabf33363ac5d06cdde3a5901f7e60794e272
rodolfoalves <rodolfosalves.pro@gmail.com>
2022-01-27 16:00:58 -0300

    configuracoes de banco feitas

--- src/App/AppBanco.java
+++ src/App/AppBanco.java
@@ -1,11 +1,13 @@
 package App;
 import Agencia.*;
 
+import java.util.Scanner;
+
 public class AppBanco {
 
-    private Agencia banco;
-    private AppCLiente appCLiente;
-    private AppGerente appGerente;
+    private static Agencia banco = new Agencia();
+    private static AppCLiente appCLiente = new AppCLiente();
+    private static AppGerente appGerente = new AppGerente();
 
     public Agencia getBanco() {
         return banco;
@@ -33,5 +35,24 @@ public class AppBanco {
 
     public static void main(String[] args){
 
+        Scanner scanner = new Scanner(System.in);
+        int menu = -1;
+
+        do{
+            System.out.println("Informe o que desejas fazer");
+            System.out.println("1 para Carregar Módulo Gerente");
+            System.out.println("2 para Carregar Módulo Cliente");
+            System.out.println("0 para sair do programa");
+            menu = Integer.parseInt(scanner.nextLine());
+
+            switch (menu){
+                case 1:
+                    appGerente.iniciarAppGerente();
+                case 2:
+                    System.out.println("teste");
+                case 0:
+                    menu = 0;
+            }
+        }while (menu != 0);
     }
 }
--- src/App/AppGerente.java
+++ src/App/AppGerente.java
@@ -1,4 +1,70 @@
 package App;
 
+import java.sql.*;
+import java.sql.Statement;
+import java.util.Scanner;
+
 public class AppGerente {
+
+    public void iniciarAppGerente(){
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Aplicativo Gerente Iniciado");
+
+        int ok = 0;
+
+        do {
+            System.out.println("Informe Login");
+            String login = scanner.nextLine();
+            System.out.println("Informe Senha");
+            String senha = scanner.nextLine();
+
+            ok = validarGerente(login, senha);
+        }while (ok == 0);
+    }
+
+    public int validarGerente(String login, String senha){
+
+        Scanner in = new Scanner(System.in);
+
+        java.sql.Connection c = null;
+        Statement stmt = null;
+
+        String login_teste = "";
+        String senha_teste = "";
+
+        try {
+            Class.forName("org.postgresql.Driver");
+            c = DriverManager.getConnection("jdbc:postgresql://localhost:5432/postgres", "postgres", "arquivo41");
+            c.setAutoCommit(false);
+
+            stmt = c.createStatement();
+
+            ResultSet rs = stmt.executeQuery( "select login,senha from gerente where gerente.login = '" + login + "';" );
+
+            while (rs.next()){
+                login_teste = String.valueOf(rs.getString("login"));
+                senha_teste = String.valueOf(rs.getString("senha"));
+                //System.out.println( "Login = " + login_teste );
+                //System.out.println( "Senha = " + senha_teste );
+            }
+
+            if (login.equals(login_teste) && senha.equals(senha_teste)){
+                return 1;
+            }
+
+            System.out.println("Login Ou Senha Incorretas");
+
+            rs.close();
+            stmt.close();
+            c.close();
+
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName()+": "+ e.getMessage() );
+            System.exit(0);
+        }
+
+        return 0;
+    }
 }
